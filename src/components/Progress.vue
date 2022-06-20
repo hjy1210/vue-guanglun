a02e13eb87eb75b8dd03a7bba014dd070ccbb7ec
Hon-Jang Yang <hjyanghj@gmail.com>
2022-06-19 17:37:28 -0700

    progress scroll bar 與音檔一致

--- src/components/Progress.vue
+++ src/components/Progress.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <button @click="playpause">{{status}}</button>
-    <audio  ref="audio1" @timeupdate="updated"></audio>
+    <audio  ref="audio1" @timeupdate="updated" @ended="ended"></audio>
     <p><span v-for= "d in listeningData" :key="d.position">{{d.position+" "}} </span></p>
     <p v-for= "d in volperiods" :key="d.volume">{{d.volume}} {{d.startTime}} {{d.endTime}} {{d.duration}} </p>
     <a href="https://www.jb51.net/article/103461.htm">https://www.jb51.net/article/103461.htm</a>
@@ -30,6 +30,7 @@
     //const source = ref([]);
     const audio1 = ref(null)
     const index = ref(0);
+    const currentTime = ref(0);
     function barmousedown(event){
       //var event = event || window.event;
       var leftVal = event.clientX - event.target.offsetLeft;
@@ -38,6 +39,7 @@
       // 拖動一定寫到 down 裡面才可以
       document.onmousemove = function(event){
         //var event = event || window.event;
+        audio1.value.pause();
         barleft.value = event.clientX - leftVal;     
         if(barleft.value < 0)
           barleft.value = 0;
@@ -45,6 +47,8 @@
           barleft.value = scroll.value.offsetWidth - bar.value.offsetWidth;
         mask.value.style.width = barleft.value +'px' ;
         bar.value.style.left = barleft.value + "px";
+        var percent = barleft.value/(scroll.value.offsetWidth-bar.value.offsetWidth);
+        setIndexCurrentTime(percent);
         txt.value = "已經走了" + parseInt(barleft.value/(scroll.value.offsetWidth-bar.value.offsetWidth) * 100) + "%";
         //console.log("已經走了" + parseInt(barleft.value/(scroll.value.offsetWidth-bar.value.offsetWidth) * 100) + "%");
         //防止选择内容--当拖动鼠标过快时候，弹起鼠标，bar也会移动，修复bug
@@ -54,6 +58,10 @@
     }
     document.onmouseup = function(){
       document.onmousemove = null; //弹起鼠标不做任何操作
+      if (status.value == "pause"){
+        status.value = "play";
+        playpause();
+      }
     }
     const volperiods=computed(()=>{
       var res = [];
@@ -76,19 +84,54 @@
         //source.value.push(leading+element.volume+'.mp3');
       });
       //res.forEach(e=>{console.log(e.duration)});
-
+      currentTime.value=res[0].startTime;
       return res;
     })
+    const duration = computed(()=>{
+      var d = 0;
+      volperiods.value.forEach(element => {
+        d+=element.duration;
+      });
+      console.log(d);
+      return d;
+    });
+    function percentage(){
+      if(index.value==0){
+        return (currentTime.value-volperiods.value[0].startTime)/duration.value;
+      } else {
+        return (volperiods.value[0].duration + currentTime.value-volperiods.value[1].startTime)/duration.value;
+      }
+    }
+    function setBarPosition(percent){
+      barleft.value = (scroll.value.offsetWidth-bar.value.offsetWidth)*percent;
+      mask.value.style.width = barleft.value +'px' ;
+      bar.value.style.left = barleft.value + "px";
+    }
+    function setIndexCurrentTime(percent){
+      var dur = duration.value * percent;
+      if (dur>volperiods.value[0].duration){
+        index.value = 1;
+        currentTime.value = dur - volperiods.value[0].duration + volperiods.value[1].startTime;
+      } else {
+        index.value = 0;
+        currentTime.value = dur + volperiods.value[0].startTime;
+      }
+    }
     function updated(){
       console.log(audio1.value.currentTime)
       console.log(volperiods.value[index.value].endTime)
+      currentTime.value = audio1.value.currentTime;
+      var percent = percentage();
+      setBarPosition(percent);
+      txt.value = "已經走了" + Math.round(percent * 100) + "%";
+
       if (audio1.value.currentTime > volperiods.value[index.value].endTime){
         audio1.value.pause();
         console.log(volperiods.value.length);
         console.log(index.value);
         if (volperiods.value.length>1 && index.value==0){
           index.value=1;
-          playindex();
+          playindex(volperiods.value[index.value].startTime);
         }
       }
     }
@@ -99,7 +142,7 @@
       console.log(index.value);
       if (volperiods.value.length>1 && index.value==0){
         index.value=1;
-        playindex();
+        playindex(volperiods.value[index.value].startTime);
       }
     }
 
@@ -111,22 +154,24 @@
       if(status.value=='play'){
         //source1.value = leading+volperiods.value[0].volume+'.mp3';
         //console.log(source.value[0].value);
-        playindex();
-        //status.value = 'pause'
+        playindex(currentTime.value);
+        status.value = 'pause'
       } else {
         audio1.value.pause();
-        //status.value='play';
+        currentTime.value = audio1.value.currentTime;
+        status.value='play';
       }
     }
-    function playindex(){
+    function playindex(currentTime){
         console.log(volperiods.value.length);
         console.log(index.value);
         console.log(volperiods.value[index.value].startTime);
         audio1.value.src = leading+volperiods.value[index.value].volume+'.mp3';
-        audio1.value.currentTime = volperiods.value[index.value].startTime;
+        //audio1.value.currentTime = volperiods.value[index.value].startTime;
+        audio1.value.currentTime = currentTime;
         audio1.value.load();
-        audio1.value.ontimeupdate=updated;
-        audio1.value.onended= ended;
+        //audio1.value.ontimeupdate=updated;
+        //audio1.value.onended= ended;
         audio1.value.play();
 
     }
