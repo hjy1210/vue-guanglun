6c26bceaad41c55fd2f7dab6a5fd0c429a608697
HonJang Yang <hjyanghj@gmail.com>
2022-09-20 16:40:36 +0800

    第一個廣論原文之前的解釋文才需要用*佔位

--- vanilla/extractGuanglun.js
+++ vanilla/extractGuanglun.js
@@ -1,6 +1,15 @@
 ///// 先將本檔案內容,抄入廣論時時聽的開發人員工具console頁面，
 ///// 執行它會把廣論原文與時間標記存成一個文字檔。
 function extractGuanglunTextOneVol() {
+    function getVolName(){
+        let div = document.querySelector("div.MuiGrid-root.MuiGrid-item");
+        let h6 = div.querySelector("h6.MuiTypography-root");
+        let text = h6.innerText;
+        let start = text.indexOf("[");
+        let end = text.indexOf("]");
+        let vol = text.substr(start+1,end-start-1);
+        return vol.toLowerCase();
+    }
     let div = document.querySelector("div.jss41.jss42")
     console.log(div.tagName);
     let res = "";
@@ -21,9 +30,10 @@ function extractGuanglunTextOneVol() {
     let blob = new Blob([str], { type: "text/plain" });
     let url = URL.createObjectURL(blob);
     let downloadlink = document.createElement('a');
-    downloadlink.download="gl-01a.txt";
+    downloadlink.download=`gl-${getVolName()}.txt`;
     downloadlink.href = url;
     downloadlink.click();
     URL.revokeObjectURL(url);
 }
 extractGuanglunTextOneVol();
+
